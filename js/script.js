eff523bae61db27049bce1f436d49b3dce06b72a
Ronarden <85498820+Ronarden@users.noreply.github.com>
2021-08-06 16:16:49 +0200

    some description and comments added to the .js file

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let x = 4; // this is a comment following a variable declaration- wow learnt a lot (inline comment!)
+/* this is a 
+gsdg
+sdg
+sdg
+multiline comment*/ 
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number and object
+*/
+// really need to keep up with my writing skills!
+
+//strings in variables:
+let familyName = "Zschirnt"; //really odd name I know 
+var fistName ="Ronny";
+const PI = 3.14;
+
+// declaring vs assigning
+//declaring a variable:
+var a;
+
+//assigning a variable:
+a = 7;
+console.log(a);
+
+// have to really look into debugging
