e16b9a94b152164e09219c6423f0ff9acf4b944b
Ronarden <85498820+Ronarden@users.noreply.github.com>
2021-08-10 21:04:26 +0200

    new file for the second week of the course

---
+++ js/boolean_values_logical_operators.js
@@ -0,0 +1,11 @@
+// Hier zeigen wir einfach die console Ausgabe, je nachdem, welchen Wert die Variable javaScriptConsole hat.
+var javaScriptCourse = true;
+if(javaScriptCourse) {
+    console.log("This is a javaScriptCourse!");
+} else {
+    console.log("This is not a Java Script Course!");
+}
+// Eventuell kann man hier noch die Variable auf true oder false mit einer Funktion setzen und so dynamisch ändern.
+
+// comparism operators
+// < > <= >
